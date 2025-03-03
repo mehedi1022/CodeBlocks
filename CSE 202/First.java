29def825bfd44e5e81922e5a9016a6e489c346af
mehedi1022 <mahdibinalamgir@gmail.com>
2025-03-04 00:45:14 +0600

    added s1.contains(freeCodeCamp)

--- CSE 202/First.java
+++ CSE 202/First.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 public class First {
 
     public static void main(String[] args) {
-        String name = "Muhammad Mehedi Hasan"; //%s
-        System.out.println(name.toUpperCase());
-        System.out.println(name.toLowerCase());
+        // String s1 = new String("abc");
+        // String s2 = new String("ABC");
+
+        // System.out.println(s1.equalsIgnoreCase(s2));
+
+        // String s1 = "The sky is blue";
+
+        // System.out.println(s1.replace("blue", "red"));
+        // System.out.println(s1);
+
+        // String update = s1.replace("blue", "red");
+        // System.out.println(update);
+
+        // System.out.println(s1.contains("freeCodeCamp"));
+
+        
     }
 }
