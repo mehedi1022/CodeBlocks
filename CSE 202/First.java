fa839b7852a48dd0989d1a76b739bb03ddff44c0
mehedi1022 <mahdibinalamgir@gmail.com>
2025-03-04 11:34:00 +0600

    added sum div sub using user input

--- CSE 202/First.java
+++ CSE 202/First.java
@@ -3,21 +3,36 @@ import java.util.Scanner;
 public class First {
 
     public static void main(String[] args) {
-        // String s1 = new String("abc");
-        // String s2 = new String("ABC");
+        Scanner s = new Scanner(System.in);
 
-        // System.out.println(s1.equalsIgnoreCase(s2));
+        System.out.println("Enter First Number: ");
+        double n1 = s.nextDouble();
 
-        // String s1 = "The sky is blue";
+        System.out.println("Enter Second Number: ");
+        double n2 = s.nextDouble();
 
-        // System.out.println(s1.replace("blue", "red"));
-        // System.out.println(s1);
+        s.nextLine();
+        System.out.println("What operation do you want to perform? ");
+        String operation = s.nextLine();
+        if (operation.equals("sum")) {
+            System.out.printf("%f + %f = %f", n1, n2, n1 + n2);
+        } else if (operation.equals("sub")) {
+            System.out.printf("%f - %f = %f", n1, n2, n1 - n2);
 
-        // String update = s1.replace("blue", "red");
-        // System.out.println(update);
+        } else if (operation.equals("mul")) {
+            System.out.printf("%f * %f = %f", n1, n2, n1 * n2);
 
-        // System.out.println(s1.contains("freeCodeCamp"));
+        } else if (operation.equals("div")) {
+            if (n2 == 0) {
+                System.out.println("Can not divide by zero");
+            }else{
+                System.out.printf("%f / %f = %f", n1, n2, n1 / n2);
+            }
 
-        
+        } else {
+            System.out.printf("%s is not supported operation", operation);
+        }
+
+        s.close();
     }
 }
